28e83915534c9a128ccc57401780e65f8082667a
Azola Mntanya <s217808921@mandela.ac.za>
2022-08-29 10:23:25 +0200

    *Added the doctor area with the style

---
+++ Prescribing System/Areas/Doctor/Controllers/PrescriptionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Prescribing_System.Areas.Doctor.Controllers
+{
+    public class PrescriptionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
